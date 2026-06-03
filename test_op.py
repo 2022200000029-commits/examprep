76b23650de9847e61e4978a9df6914145effd36b
Pranto <mehrabpranto.fb@gmail.com>
2026-06-03 15:26:34 +0600

    op and op test add

---
+++ test_op.py
@@ -0,0 +1,24 @@
+from op import add, subtract, multiply, divide
+
+
+def test_add():
+    a = 10
+    b = 5
+    assert add(a,b) == 15
+
+
+def test_subtract():
+    a=10
+    b=5
+    assert subtract(a,b)==5
+
+def test_multiply():
+    a=10
+    b=5
+    assert multiply(a,b)== 50
+
+def test_divide():
+    a=10
+    b=5
+    assert divide(a,b)==2
+    
